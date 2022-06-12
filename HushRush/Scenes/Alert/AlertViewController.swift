c4293c171774425d8f3d17c97864c47c0f20a576
Zidan Ramadhan <zidan.ramadhan@ui.ac.id>
2022-06-12 19:12:58 +0700

    initial commit

---
+++ HushRush/Scenes/Alert/AlertViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HushRush
+//
+//  Created by Zidan Ramadhan on 12/06/22.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
