78d27389d861b1becda42eb7eefd4991ca63a1ae
wilbertdevin <wilbertdevin888@gmail.com>
2022-06-20 15:43:04 +0700

    Set Up Profile View

--- HushRush/Scenes/Onboarding/OnboardingViewController.swift
+++ HushRush/Scenes/Onboarding/OnboardingViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var birthDateField: UITextField!
+    @IBOutlet weak var deafPickerField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var skipButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
